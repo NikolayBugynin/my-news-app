0dc36d1ba4b65b8d53786192ab91e8ba15265812
Nikolay Bugynin <bugynin@yandex.ru>
2026-03-28 13:50:12 +0300

    add skeletom for news and banner

---
+++ src/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,27 @@
+import styles from './styles.module.css';
+
+interface SkeletonrProps {
+  count?: number;
+  type?: 'banner' | 'item';
+}
+
+export const Skeleton = ({ count = 1, type = 'banner' }: SkeletonrProps) => {
+  const skeletonClass = type === 'banner' ? styles.banner : styles.item;
+  return (
+    <>
+      {count > 1 ? (
+        <ul className={styles.list}>
+          {Array.from({ length: count }).map((_, index) => (
+            <li
+              key={index}
+              className={skeletonClass}
+              aria-label='Загрузка контента'
+            ></li>
+          ))}
+        </ul>
+      ) : (
+        <li className={skeletonClass} aria-label='Загрузка контента'></li>
+      )}
+    </>
+  );
+};
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,17 +2,23 @@ import { useEffect, useState } from 'react';
 import { getNews } from '../../api/apiNews';
 import { NewsBanner } from '../../components/NewsBanner/NewsBanner';
 import { NewsList } from '../../components/NewsList/NewsList';
+import { Skeleton } from '../../components/Skeleton/Skeleton';
 import styles from './styles.module.css';
 
 // interface MainProps {}
 
 export const Main = () => {
   const [news, setNews] = useState([]);
+
+  const [isLoading, setIsloading] = useState(true);
+
   useEffect(() => {
     const fetchNews = async () => {
       try {
+        setIsloading(true);
         const response = await getNews();
         setNews(response.news);
+        setIsloading(false);
       } catch (error) {
         console.error(error);
       }
@@ -21,8 +27,16 @@ export const Main = () => {
   }, []);
   return (
     <main className={styles.main}>
-      {news.length > 0 ? <NewsBanner item={news[0]} /> : null}
-      <NewsList news={news} />
+      {news.length > 0 && !isLoading ? (
+        <NewsBanner item={news[0]} />
+      ) : (
+        <Skeleton count={1} type={'banner'} />
+      )}
+      {!isLoading ? (
+        <NewsList news={news} />
+      ) : (
+        <Skeleton count={10} type={'item'} />
+      )}
     </main>
   );
 };
